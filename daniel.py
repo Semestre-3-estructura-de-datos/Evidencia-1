e7447a2bd7b0d4f1687e0ee132dae94eae1e7d5b
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-11-03 19:17:35 -0600

    Cambios

--- daniel.py
+++ daniel.py
@@ -2,6 +2,7 @@ import shutil
 
 def copiar(nombre):
     shutil.copy('Documento.txt',nombre)
+    print("El Documento ya esta copiado :)")
 
 def mover():
     shutil.move('Documento.txt','archivo_mover',copy_function=shutil.copy)
